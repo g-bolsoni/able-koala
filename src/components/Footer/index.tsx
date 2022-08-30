d9169dbe0adaaa13bdadb9c203096c72284637b3
g-bolsoni <giovanebolsoni2@gmail.com>
2022-08-29 21:39:34 -0300

    Create a footer component and add the logos images

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './style.module.css'
+
+export default function Footer() {
+  return (
+    <>
+      <h1 className={styles.footer}>footer Word</h1>
+    </>
+  )
+}
