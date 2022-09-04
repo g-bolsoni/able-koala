036c8d1cd5f7ab9a82df06f98192bcc38fa88ecf
Eliabe Correa <eliabe.correa@irroba.com.br>
2022-09-04 20:12:23 -0300

    menu mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './style.module.sass'
 import logo from './images/logos/2.png'
+import { HiMenu, HiOutlineMail } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
-export default function Footer() {
+export default function Header() {
+
+    const [isActive, setActive] = useState(false);
+    let isMobile = false
+
+    if (screen.width < 992) {
+        isMobile = true
+    }
+
+    const toggleClassMenuBtn = () => {
+        setActive(!isActive);
+    };
+  
+
   return (
     <>
         <nav className={styles.header}>
@@ -19,17 +35,22 @@ export default function Footer() {
             </div>
 
             <div className={styles.menu}>
-                <ul className={styles.links}>
-                    <li className={styles.link}><Link href="/home">Home</Link></li>
-                    <li className={styles.link}><Link href="/about-us">About Us</Link></li>
-                    <li className={styles.link}><Link href="/services">Services</Link></li>
-                    <li className={styles.link}><Link href="/carrers">Carrers</Link></li>
-                    <li className={styles.link}><Link href="/contact-us">Contact Us</Link></li>
-                </ul>
+                <button className={styles.btn_menu} onClick={toggleClassMenuBtn}>
+                    {isActive ? <AiOutlineClose /> : <HiMenu/>}   
+                </button>
+                <div className={isActive ? styles.active : null}>
+                    <ul className={styles.links}>
+                        <li className={styles.link}><Link href="/home">Home</Link></li>
+                        <li className={styles.link}><Link href="/about-us">About Us</Link></li>
+                        <li className={styles.link}><Link href="/services">Services</Link></li>
+                        <li className={styles.link}><Link href="/carrers">Carrers</Link></li>
+                        <li className={styles.link}><Link href="/contact-us">Contact Us</Link></li>
+                    </ul>
+                </div>
             </div>
             <div className={styles.contact}>
                 <Link href="mailto:info@ablekoala.com.au" className={styles.email_link}>
-                    e info@ablekoala.com.au
+                    {isMobile ? <HiOutlineMail /> : 'e info@ablekoala.com.au'}
                 </Link>
             </div>
             
