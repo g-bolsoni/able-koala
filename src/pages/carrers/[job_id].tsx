3170720bfbc9dbd3318507263f0e93004b982770
g-bolsoni <giovanebolsoni2@gmail.com>
2023-06-15 01:48:12 -0300

    Adicionando works

--- src/pages/carrers/[job_id].tsx
+++ src/pages/carrers/[job_id].tsx
@@ -11,6 +11,7 @@ import { sendContactMail } from '../../services/sendMail';
 import toast from 'react-hot-toast';
 import { storage } from '../../services/firebaseConection';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import LoadingButton from '../../components/Loading';
 
 export default function index() {
   // const [modalOpen, setmodalOpen] = useState(false);
@@ -50,8 +51,7 @@ export default function index() {
     return urlFile;
   }
 
-  async function handleSubmit(e: FormEvent) {
-    e.preventDefault();
+  async function handleSubmit() {
 
     if(!email.trim() || !name.trim() || !number.trim() ) {
       toast.error('Preencha todos os campos', {style: {
@@ -91,85 +91,159 @@ export default function index() {
   }
 
   return (
-    <>
-      <div className="container">
-        <Link href="/carrers" passHref>
-          <span className={styles.back}>
-            <AiOutlineArrowLeft/>
-            Back
-          </span>
-        </Link>
-        {works.works.map(work => {
-            if( `${work.id}` == jobId ) {
-              return (
-                <main key={work.id} className={styles.work_container}>
-                  <h1>{work.title}</h1>
-                  <section> 
+    <div className="container">
+      <Link href="/carrers" passHref>
+        <span className={styles.back}>
+          <AiOutlineArrowLeft/>
+          Back
+        </span>
+      </Link>
+
+      {works.works.map(work => {
+          if( `${work.id}` == jobId ) {      
+            return (
+              <main key={work.id} className={styles.work_container}>
+                <h1>{work.title}</h1>
+                {/* Descrição da vaga */}
+                <section className={styles.section_description}> 
+                  <h2 className={styles.description_title}>
+                    Job Description
+                  </h2>
+                  
+                  <p>{work.description}</p>
+                  <p className={styles.subtitle}>
+                    {work.description_list_title ? work.description_list_title : `What will be needed ?`} 
+                  </p>
+                  <ul className={styles.description_list}>
+                    {
+                      work.requirements.map(requirement => {
+                        return(
+                        <li key={requirement.id} className={styles.list_requirement}>
+                          {requirement.requirement}
+                        </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </section>
+                {/* Descrição da vaga */}
+                {/* Tipo da vaga */}
+                <section className={styles.section_description}> 
+                  <h2 className={styles.description_title}>
+                    Type Of Employment
+                  </h2>
+                  <p>{work.type_of_employment}</p>
+                </section>
+                {/* Tipo da vaga */}
+                {/* Chefe */}
+                {work.report_to ?
+                  <section className={styles.section_description}> 
                     <h2 className={styles.description_title}>
-                      Description
+                      Report To
                     </h2>
-                    <p>{work.description}</p>
+                    <p>{work.report_to}</p>
                   </section>
-                  <section className={styles.requirements}>
-                    <h2 className={styles.requirements_title}>What will be needed ?</h2>
-                    <ul>
-                      {
-                        work.requirements.map(requirement => {
-                          return(
-                          <li key={requirement.id} className={styles.list_requirement}>
-                            {requirement.requirement}
-                          </li>
-                          )
-                        })
-                      }
-                    </ul>
+                  : ''
+                }
+                {/* Chefe */}
+                {/* Localização */}
+                {work.location ?
+                  <section className={styles.section_description}> 
+                    <h2 className={styles.description_title}>
+                      Location
+                    </h2>
+                    <p>{work.location}</p>
                   </section>
-                </main>
-              )
-            }
-        })}
-
-        {/* <button onClick={() => setmodalOpen(true)} className={styles.button}>I WANT APPLY </button> */}
-        
-        <form className={styles.jobs_form}  onSubmit={handleSubmit}>
-          <div className={styles.row}>
-            <div>
-              <input type="text" name="name" required value={name} onChange={(e) => setName(e.target.value)}/>
-              <span className={styles.text} >Full Name</span>
-              <span className={styles.line}></span>
-            </div>
-            <div>
-              <input type="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
-              <span className={styles.text}>Your E-mail</span>
-              <span className={styles.line}></span>
-            </div>
+                  : ''
+                }
+                {/* Localização */}
+                {/* Responsabilidades */}
+                <section className={styles.section_description}> 
+                  <h2 className={styles.description_title}>
+                    Main Duties / Responsibilities
+                  </h2>
+                  <p>{work.main_responsibilities_text ?? work.main_responsibilities_text}</p>
+                  <div className={styles.content}>
+                    {work.main_responsibilities.map(responsability => (
+                      <div key={responsability.id}>
+                        <p><b>{responsability.name}</b></p>
+                        <ul className='responsibilities_list'>
+                          {responsability.responsibilities.map(responsibilities => (
+                            <li key={responsibilities.id}>
+                              {responsibilities.responsibility}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ))}
+                  </div>
+                </section>
+                {/* Responsabilidades */}
+                {/* Qualifications/ Requirements */}
+                <section className={styles.section_description}> 
+                  <h2 className={styles.description_title}>
+                    Qualifications/ Requirements
+                  </h2>
+                  <p>{work.requirements_title ?? work.requirements_title}</p>
+                  <ul className='responsibilities_list'>
+                    {work.requirements.map(requirement =>(
+                      <li key={requirement.id}>
+                        {requirement.requirement}
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+                {/* Qualifications/ Requirements */}
+                {/* Sills */}
+                <section className={styles.section_description}> 
+                  <h2 className={styles.description_title}>
+                    Skills
+                  </h2>
+                  <p>{work.requirements_title ?? work.requirements_title}</p>
+                  <ul className='responsibilities_list'>
+                    {work.skills.map(skill =>(
+                      <li key={skill.id}>
+                        {skill.requirement}
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+                {/* Skills */}
+              </main>
+            )
+          }
+      })}
+      
+      <form className={styles.jobs_form}>
+        <div className={styles.row}>
+          <div>
+            <input type="text" name="name" required value={name} onChange={(e) => setName(e.target.value)}/>
+            <span className={styles.text} >Full Name</span>
+            <span className={styles.line}></span>
           </div>
-          <div className={styles.row}>
-            <div>
-              <input type="number" name="number" required value={number}  onChange={(e) => setNumber(e.target.value)}/>
-              <span className={styles.text}>Cellphone Number</span>
-              <span className={styles.line}></span>
-            </div>
-            <div>
-              <input type="file" accept="application/pdf" onChange={(e) => {
-                setFile(e.target.files[0])
-              }} /> 
-             
-              <span className={styles.text}>Your CV</span>
-              <span className={styles.line}></span>
-            </div>
+          <div>
+            <input type="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <span className={styles.text}>Your E-mail</span>
+            <span className={styles.line}></span>
           </div>
-
-          <button 
-            type="submit" 
-            className={`${styles.submit_button}`} 
-            disabled={loading}
-          >
-            Send
-          </button>
-        </form>
-
-      </div>
-    </>
+        </div>
+        <div className={styles.row}>
+          <div>
+            <input type="number" name="number" required value={number}  onChange={(e) => setNumber(e.target.value)}/>
+            <span className={styles.text}>Cellphone Number</span>
+            <span className={styles.line}></span>
+          </div>
+          <div>
+            <input type="file" accept="application/pdf" onChange={(e) => {
+              setFile(e.target.files[0])
+            }} /> 
+            
+            <span className={styles.text}>Your CV</span>
+            <span className={styles.line}></span>
+          </div>
+        </div>
+        <LoadingButton onClick={handleSubmit} loading={loading} text="Send" />
+      </form>
+    </div>
   )
 }
--- src/pages/carrers/index.tsx
+++ src/pages/carrers/index.tsx
@@ -14,10 +14,11 @@ export default function carrers() {
         <div className={styles.disponibility_works}>
         {
                 works.works.map(work => (
+
                 <div className={styles.work_list} key={work.id} >
                     <div className={styles.work}>
                         <h2 className={styles.work_title}>{work.title}</h2>
-                        <span className={styles.location}>{work.location}</span>
+                        {work.location ?? <span className={styles.location}>{work.location}</span> }
                     </div>
                     <Link href={`/carrers/${work.id}`} passHref >
                         <div className={styles.read_more}>
@@ -25,6 +26,7 @@ export default function carrers() {
                         </div>
                     </Link>
                 </div>
+                
                 ))
             }
         </div>
