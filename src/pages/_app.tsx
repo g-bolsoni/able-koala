46946924d381256cf06c422a7d753d95fcb2509c
g-bolsoni <giovanebolsoni2@gmail.com>
2023-07-12 18:24:59 -0300

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../../styles/globals.sass'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { AppProvider } from '../data/context/AppContext'
@@ -7,6 +8,7 @@ import Loading from '../components/Modals/Loading'
 export default function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
+      <Analytics />
       <Loading />
       <Toaster position='top-right' />
       <Header />
