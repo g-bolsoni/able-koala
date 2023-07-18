2089d6f8deadf4b596443dbace16c6a5504b1e41
g-bolsoni <giovanebolsoni2@gmail.com>
2023-07-18 19:11:01 -0300

    Adicionando keywords

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
                     <meta name="language" content="en-US" />
                     <meta name="description" content="This book helps caregivers, family members, and friends (relatives) of the elderly and people with special needs to understand the need and importance of taking care of each other."/>
+                    <meta name="keywords" content="STA,SIL,Disabled, Short term accomodation, Disability Holiday, Able koala, Koala care, Care service 24/7, 24/7 high level care, Personal care, Community service, Wheelchair accessible car, Transport, Electric Wheelchair transport, Domestic assistance, Yard maintenance, Support coordinator, Plan Manage, Lovely cares, Care with love, High qualified support worker, Short term accomodation disabled holiday, Sta holiday, Sta disabled holiday, Gold Coast disability/disabled holiday, Sydney disability /disable holiday, Able Koala, Able koala, able koala, able koala, ablekoala" />
                     <meta name="robots" content="index, follow" />
                     <meta name="googlebot" content="index, follow" />
                     <meta name="geo.region" content="Au" />
