5050d4b6eec9ead89bb4c5639da19d0238e29cea
g-bolsoni <giovanebolsoni2@gmail.com>
2023-12-11 21:08:56 -0300

    Add tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,22 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { AppProvider } from '../data/context/AppContext'
 import Loading from '../components/Modals/Loading'
+
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
+
+
+
 export default function MyApp({ Component, pageProps }) {
+
+  useEffect(()=>{
+    const tagManagerArgs = {
+      gtmId: 'GTM-5TQPWMXB'
+    }
+
+    TagManager.initialize(tagManagerArgs);
+  }, [])
+
   return (
     <AppProvider>
       <Analytics />
