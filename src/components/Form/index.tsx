b4d6f26302a9249ae1243bebaeeda09b3e7ec57c
g-bolsoni <giovanebolsoni2@gmail.com>
2023-12-12 21:58:42 -0300

    base for change the forms

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,58 +7,54 @@ import LoadingButton from "../Loading";
 
 export default function Form() {
 
-    const { postContactUs }                                               = useAppData();
-    const [ndisName, setNdisName]                                         = useState("");
-    const [ndisEmail, setNdisEmail]                                       = useState("");
-    const [ndisPhone, setNdisPhone]                                       = useState("");
-    const [specializedEquipmentDetails, setSpecializedEquipmentDetails]   = useState("");
+    const { postContactUs } = useAppData();
+    const [ndisName, setNdisName] = useState("");
+    const [ndisEmail, setNdisEmail] = useState("");
+    const [ndisPhone, setNdisPhone] = useState("");
+    const [specializedEquipmentDetails, setSpecializedEquipmentDetails] = useState("");
     const [additional_requirements_text, setAdditional_requirements_text] = useState("");
-    const [caregiverPersonName, setCaregiverPersonName]                   = useState("");
-    const [subscriptionServicePerson, setSubscriptionServicePerson]       = useState("");
-    const [accomodation, setAccomodation]                                 = useState("");
-    const [checkInDate, setCheckInDate]                                   = useState("");
-    const [checkOutDate, setCheckOutDate]                                 = useState("");
-    const [managerEmail, setManagerEmail]                                 = useState("");
-    const [coordinatorEmail, setCoordinatorEmail]                         = useState("");
-    const [coordinatorName, setCoordinatorName]                           = useState("");
-    const [coordinatorPhone, setCoordinatorPhone]                         = useState("");
-    const [numberBedrooms, setNumberBedrooms]                             = useState(0);
-    const [specificRequirements, setSpecificRequirements]                 = useState("standard_room");
-    const [isParkingRequired, setIsParkingRequired]                       = useState("no");
-    const [isCarTransfers, setIsCarTransfers]                             = useState("no");
-    const [specializedEquipment, setSpecializedEquipment]                 = useState("hospitalBed");
-    const [isOtherPerson, setIsOtherPerson]                               = useState("no");
-    const [planManaged, setPlanManaged]                                   = useState("0");
-    const [isSupportCoordinator, setIsSupportCoordinator]                 = useState("no");
+    const [caregiverPersonName, setCaregiverPersonName] = useState("");
+    const [subscriptionServicePerson, setSubscriptionServicePerson] = useState("");
+    const [accomodation, setAccomodation] = useState("");
+    const [checkInDate, setCheckInDate] = useState("");
+    const [checkOutDate, setCheckOutDate] = useState("");
+    const [managerEmail, setManagerEmail] = useState("");
+    const [numberBedrooms, setNumberBedrooms] = useState(0);
+    const [specificRequirements, setSpecificRequirements] = useState("standard_room");
+    const [isParkingRequired, setIsParkingRequired] = useState("no");
+    const [isCarTransfers, setIsCarTransfers] = useState("no");
+    const [specializedEquipment, setSpecializedEquipment] = useState("hospitalBed");
+    const [isOtherPerson, setIsOtherPerson] = useState("no");
+    const [planManaged, setPlanManaged] = useState("0");
+    const [isSupportCoordinator, setIsSupportCoordinator] = useState("no");
+    const [coordinatorEmail, setCoordinatorEmail] = useState("");
+    const [coordinatorName, setCoordinatorName] = useState("");
+    const [coordinatorPhone, setCoordinatorPhone] = useState("");
 
     const [loading, setLoading] = useState(false);
 
     const setPostContactUs = async () => {
         setLoading(true);
         await postContactUs!(contactUsSendToJson({
-            NDIS_name                   : ndisName,
-            email                       : ndisEmail,
-            phone                       : ndisPhone,
-            bedrooms                    : numberBedrooms,
-            specific_room               : specificRequirements,
-            support_coodinator          : isSupportCoordinator,
-            coodinator_name             : coordinatorName,
-            coodinator_email            : coordinatorEmail,
-            coodinator_phone            : coordinatorPhone,
-            family_member               : isOtherPerson,
-            contact_person1             : caregiverPersonName,
-            contact_person2             : subscriptionServicePerson,
-            STA_accomodation            : accomodation,
-            STA_check_in_date           : checkInDate,
-            STA_check_out_date          : checkOutDate,
-            NDIS_plan                   : planManaged,
-            manager_email               : managerEmail,
-            specializedEquipmentDetails : specializedEquipmentDetails,
-            specializedEquipment        : specializedEquipment,
+            NDIS_name: ndisName,
+            email: ndisEmail,
+            phone: ndisPhone,
+            bedrooms: numberBedrooms,
+            specific_room: specificRequirements,
+            family_member: isOtherPerson,
+            contact_person1: caregiverPersonName,
+            contact_person2: subscriptionServicePerson,
+            STA_accomodation: accomodation,
+            STA_check_in_date: checkInDate,
+            STA_check_out_date: checkOutDate,
+            NDIS_plan: planManaged,
+            manager_email: managerEmail,
+            specializedEquipmentDetails: specializedEquipmentDetails,
+            specializedEquipment: specializedEquipment,
             additional_requirements_text: additional_requirements_text,
-            isParkingRequired           : isParkingRequired,
-            isCarTransfers              : isCarTransfers,
-            origin                      : "services"
+            isParkingRequired: isParkingRequired,
+            isCarTransfers: isCarTransfers,
+            origin: "services"
 
         }));
         setLoading(false);
@@ -93,14 +89,14 @@ export default function Form() {
 
                     <div className={style.input_group}>
                         <label htmlFor="specificRequirements">Please, specific room requirements</label>
-                        <select name="specificRequirements" id="specificRequirements" defaultValue={specificRequirements}  onChange={(e) => setSpecificRequirements(e.target.value)}>
+                        <select name="specificRequirements" id="specificRequirements" defaultValue={specificRequirements} onChange={(e) => setSpecificRequirements(e.target.value)}>
                             <option value="standard_room" selected>Standard Room</option>
                             <option value="accessible_room">Accessible Room</option>
                             <option value="additional_requirements">Additional requirements</option>
                         </select>
                     </div>
 
-                    { specificRequirements == 'additional_requirements' && (
+                    {specificRequirements == 'additional_requirements' && (
                         <div className={style.input_group}>
                             <label htmlFor="">Please, detail the room requirements </label>
                             <input type="text" name="additional_requirements_text" value={additional_requirements_text} id="additional_requirements_text" onChange={(e) => setAdditional_requirements_text(e.target.value)} />
@@ -109,7 +105,7 @@ export default function Form() {
 
                     <div className={style.input_group}>
                         <label htmlFor="specializedEquipment">Will you need specialized equipment</label>
-                        <select name="specializedEquipment"  defaultValue={specializedEquipment} id="specializedEquipment" onChange={(e) => setSpecializedEquipment(e.target.value)} >
+                        <select name="specializedEquipment" defaultValue={specializedEquipment} id="specializedEquipment" onChange={(e) => setSpecializedEquipment(e.target.value)} >
                             <option value="hospitalBed" selected >Hospital bed</option>
                             <option value="staticShowerChair">Static shower chair</option>
                             <option value="commodeShowerChair">Commode shower chair</option>
@@ -119,38 +115,11 @@ export default function Form() {
                         </select>
                     </div>
 
-                    { specializedEquipment == 'none' && (
+                    {specializedEquipment == 'none' && (
                         <div className={style.input_group}>
                             <label htmlFor="specializedEquipmentDetails">Please, specify requirements</label>
                             <input type="text" name="specializedEquipmentDetails" value={specializedEquipmentDetails} id="specializedEquipmentDetails" onChange={(e) => setSpecializedEquipmentDetails(e.target.value)} />
                         </div>
-                    )}
-
-                    <div className={style.input_group}>
-                        <label htmlFor="isSupportCoordinator">Do you have a support coordinator?</label>
-                        <select name="isSupportCoordinator"  defaultValue={isSupportCoordinator} id="isSupportCoordinator" onChange={(e) => setIsSupportCoordinator(e.target.value)} >
-                            <option value="no" selected >No</option>
-                            <option value="yes">Yes</option>
-                        </select>
-                    </div>
-
-                    {isSupportCoordinator == "yes" && (
-                        <>
-                            <div className={style.input_group}>
-                                <label htmlFor="coordinatorName">Support coordinator name</label>
-                                <input type="text" name="coordinatorName" value={coordinatorName} id="coordinatorName" onChange={(e) => setCoordinatorName(e.target.value)} />
-                            </div>
-
-                            <div className={style.input_group}>
-                                <label htmlFor="coordinatorEmail">Support coordinator email</label>
-                                <input type="email" name="coordinatorEmail" value={coordinatorEmail} id="coordinatorEmail" onChange={(e) => setCoordinatorEmail(e.target.value)} />
-                            </div>
-
-                            <div className={style.input_group}>
-                                <label htmlFor="coordinatorPhone">Support coordinator phone</label>
-                                <ReactInputMask mask={'999 999 999'} type="text" name="coordinatorPhone" value={coordinatorPhone} id="coordinatorPhone" onChange={(e) => setCoordinatorPhone(e.target.value)} />
-                            </div>
-                        </>
                     )}
 
 
--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -3,20 +3,20 @@ import { createContext, useEffect, useState } from "react"
 import { ContactUsSend } from "../../models/contact_us_send"
 import { ContactUs } from "../../models/page_contact_us"
 import { db } from "../../services/firebaseConection"
-import { sendContactUs,contactUs } from '../../services/sendMail';
+import { sendContactUs, contactUs } from '../../services/sendMail';
 import { toast } from "react-hot-toast";
 
 interface AppContextProps {
-    postContactUs?: (contactUs:ContactUsSend) => void
+    postContactUs?: (contactUs: ContactUsSend) => void
     loading?: boolean
     setLoading?: (value: boolean) => void
     children?: any,
-    sendContact?: (contact:ContactUs) => void
+    sendContact?: (contact: ContactUs) => void
 }
 
 const AppContext = createContext<AppContextProps>({})
 
-export function AppProvider(props:AppContextProps){
+export function AppProvider(props: AppContextProps) {
 
     const [loading, changeLoading] = useState(false)
 
@@ -24,28 +24,28 @@ export function AppProvider(props:AppContextProps){
         changeLoading(value)
     }
 
-    async function postContactUs(contactUs:ContactUsSend){
+    async function postContactUs(contactUs: ContactUsSend) {
         try {
-            await addDoc(collection(db, "contact"),contactUs); 
+            await addDoc(collection(db, "contact"), contactUs);
             await sendContactUs(contactUs);
 
-            toast.success('Send e-mail Sucessfully!')                  
+            toast.success('Send e-mail Sucessfully!')
         } catch (error) {
             toast.error('Email Failed!');
-            
-            console.log(error);                
-        }        
+
+            console.log(error);
+        }
     }
 
-    async function sendContact(contact:ContactUs){
-        try {            
-            await addDoc(collection(db, "contact_us"),contact);
+    async function sendContact(contact: ContactUs) {
+        try {
+            await addDoc(collection(db, "contact_us"), contact);
             await contactUs(contact);
-            toast.success('Send e-mail Sucessfully!')                  
+            toast.success('Send e-mail Sucessfully!')
         } catch (error) {
             toast.error('Email Failed!');
-            console.log(error);                
-        }        
+            console.log(error);
+        }
     }
 
 
